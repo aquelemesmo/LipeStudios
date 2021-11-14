d51f91927b7b14bcc35228ca5fc35f028a7cf5d4
aquelemesmoojack <65928307+aquelemesmo@users.noreply.github.com>
2021-11-14 02:46:41 -0300

    Add files via upload

---
+++ src/commands/db/daily.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+const db = require("quick.db")
+const ms = require("parse-ms")
+
+module.exports.run = async (bot, message, args) => {
+    const membro = message.mentions.members.first() || message.author;
+
+    let timetout = 86400000;
+    let quantia = Math.floor(Math.random() * 50000)
+
+    let diaria = await db.fetch(`diaria_${membro.id}`)
+
+    if(diaria !== null && timetout - (Date.now() - diaria) > 0) {
+        let tempo = ms(timetout - (Date.now() - diaria))
+    
+        const tempoEmbed = new MessageEmbed()
+        .setColor("PURPLE")
+        .setDescription("Você já pegou sua recompensa diária! e agora terá que esperar. " + tempo.hours + "h " + tempo.minutes + "m " + tempo.seconds + "s ")
+        message.reply({embeds: [tempoEmbed]})
+    } else {
+        const sucessEmbed = new MessageEmbed()
+        .setColor("PURPLE")
+        .setDescription("Você coletou sua recompensa diária! e ganhou " + quantia)
+        message.reply({embeds: [sucessEmbed]})
+        db.add(`moedinhas_${membro.id}`, quantia)
+        db.set(`diaria_${membro.id}`, Date.now())
+    }
+}
+
+module.exports.help = {
+    name: "daily",
+    aliases: ["recompensa-diaria", "diaria"]
+}
---
+++ src/commands/game/snake.js
@@ -0,0 +1,26 @@
+const { Snake } = require("discord-gamecord")
+
+module.exports.run = async (bot,message,args) => {
+    new Snake({
+        message: message,
+        embed: {
+            title: "Jogo da cobrinha",
+            color: "PURPLE",
+            OverTitle: 'Game Over',
+        },
+        snake: { head: '🟢', body: '🟩', tail: '🟢' },
+        emojis: {
+            board: '⬛',
+            food: '🍎',
+            up: '⬆️', 
+            down: '⬇️',
+            right: '➡️',
+            left: '⬅️',
+        }
+    }).startGame();
+}
+
+module.exports.help = {
+    name: "snake",
+    aliases: ["cobra"]
+}
