c51380ed21c2d06bb5d130a868c3c13cafaa326e
aquelemesmoojack <65928307+aquelemesmo@users.noreply.github.com>
2021-12-12 10:49:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,95 +1,29 @@
 const { Client, Collection } = require("discord.js")
-const bot = new Client({intents: 32767});
-const { readdir } = require("fs")
+const bot = new Client({intents: 32767, ws: {properties: {$browser: "Discord iOS"}}});
+const { readdir, readdirSync } = require("fs")
 const config = require("./src/json/config.json")
 const { DiscordTogether } = require("discord-together")
 bot.discordTogether = new DiscordTogether(bot)
 bot.commands = new Collection();
 bot.aliases = new Collection();
 
-readdir("./src/commands/admin", (err, files) => {
-    if(err) console.error(err)
-    let arquivojs = files.filter(f => f.split(".").pop() === "js")
-    arquivojs.forEach((f, i) => {
-        let props = require(`./src/commands/admin/${f}`)
-        bot.commands.set(props.help.name, props)
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.aliases)
-        })
-    })
-})
-
-readdir("./src/commands/auth", (err, files) => {
-    if(err) console.error(err)
-    let arquivojs = files.filter(f => f.split(".").pop() === "js")
-    arquivojs.forEach((f, i) => {
-        let props = require(`./src/commands/auth/${f}`)
-        bot.commands.set(props.help.name, props)
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.aliases)
-        })
-    })
-})
-
-readdir("./src/commands/minecraft", (err, files) => {
-    if(err) console.error(err)
-    let arquivojs = files.filter(f => f.split(".").pop() === "js")
-    arquivojs.forEach((f, i) => {
-        let props = require(`./src/commands/minecraft/${f}`)
-        bot.commands.set(props.help.name, props)
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.aliases)
-        })
-    })
-})
-
-readdir("./src/commands/music", (err, files) => {
-    if(err) console.error(err)
-    let arquivojs = files.filter(f => f.split(".").pop() === "js")
-    arquivojs.forEach((f, i) => {
-        let props = require(`./src/commands/music/${f}`)
-        bot.commands.set(props.help.name, props)
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.aliases)
-        })
-    })
-})
-
-readdir("./src/commands/utils", (err, files) => {
-    if(err) console.error(err)
-    let arquivojs = files.filter(f => f.split(".").pop() === "js")
-    arquivojs.forEach((f, i) => {
-        let props = require(`./src/commands/utils/${f}`)
-        bot.commands.set(props.help.name, props)
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.aliases)
-        })
-    })
-})
-
-readdir("./src/commands/config", (err, files) => {
-    if(err) console.error(err)
-    let arquivojs = files.filter(f => f.split(".").pop() === "js")
-    arquivojs.forEach((f, i) => {
-        let props = require(`./src/commands/config/${f}`)
-        bot.commands.set(props.help.name, props)
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.aliases)
-        })
-    })
-})
-
-readdir("./src/commands/db", (err, files) => {
-    if(err) console.error(err)
-    let arquivojs = files.filter(f => f.split(".").pop() === "js")
-    arquivojs.forEach((f, i) => {
-        let props = require(`./src/commands/db/${f}`)
-        bot.commands.set(props.help.name, props)
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.aliases)
-        })
-    })
-})
+readdirSync('./src/commands/').forEach(dir => {
+    readdir(`./src/commands/${dir}`, (err, files) => {
+        if (err) throw err;
+        let jsFiles = files.filter(f => f.split(".").pop() === "js");
+        jsFiles.forEach(file => {
+            let props = require(`./src/commands/${dir}/${file}`);
+            try {
+                bot.commands.set(props.help.name, props);
+                props.help.aliases.forEach(alias => {
+                    bot.aliases.set(alias, props.help.name);
+                })
+            } catch (err) {
+                return console.log(err);
+            }
+        });
+    });
+});
 
 readdir("./src/events", (err, files) => {
     if (err) console.error(err);
