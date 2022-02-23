80f0ab6d4aa1a69efbaa51dfc19a1cfcb2a1a5d8
aquelemesmoojack <65928307+aquelemesmo@users.noreply.github.com>
2022-02-22 21:36:34 -0300

    Add files via upload

---
+++ src/commands/config/setleave.js
@@ -0,0 +1,26 @@
+const mensagem = require("../../json/mensagem.json")
+const { MessageEmbed } = require("discord.js")
+const { Database } = require("simpl.db")
+const db = new Database()
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.permissions.has("MANAGE_SERVER")) {return message.reply(mensagem.semPermissão)}
+    if(!message.guild.me.permissions.has("MANAGE_SERVER")) {return message.reply(mensagem.semPermissãoGuild)}
+
+    let channel = message.mentions.channels.first()
+
+    if(!channel) {return message.reply("Você tem que mencionar um canal de texto!")}
+    
+    db.set(`leavechannel_${message.guild.id}`, channel.id)
+
+    const embed = new MessageEmbed()
+    .setColor("PURPLE")
+    .setTitle("Sucesso!")
+    .setDescription("Adicionado mensagem de saída no canal: " + channel.name)
+    message.reply({embeds: [embed]})
+}
+
+module.exports.help = {
+    name: "setleave",
+    aliases: ["setsaída"]
+}
